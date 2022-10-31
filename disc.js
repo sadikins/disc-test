4f044a7e99134a2595d146e3b5ff4f51a4aa156e
sadikins <msadikin1989@gmail.com>
2022-10-31 17:05:22 +0700

    first commit

---
+++ disc.js
@@ -0,0 +1,610 @@
+/**===================
+ * ===== Database ====
+ * ===================
+ */
+
+const database = [
+    {
+        pernyataan_1: 'Mudah bergaul, ramah.',
+        pernyataan_2: 'Penuh kepercayaan, percaya kepada orang lain.',
+        pernyataan_3: 'Petualang, pengambil resiko.',
+        pernyataan_4: 'Toleran, Penuh hormat.',
+        p_1: 's',
+        p_2: 'i',
+        p_3: 'x',
+        p_4: 'c',
+        k_1: 's',
+        k_2: 'i',
+        k_3: 'd',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Lembut, pendiam',
+        pernyataan_2: 'Optimis, Pengkhayal.',
+        pernyataan_3: 'Pusat perhatian, Mudah bersosialisasi.',
+        pernyataan_4: 'Pembuat perdamaian, Membawa ketenagan.',
+        p_1: 's',
+        p_2: 'i',
+        p_3: 'x',
+        p_4: 'c',
+        k_1: 's',
+        k_2: 'i',
+        k_3: 'd',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Mendorong orang lain.',
+        pernyataan_2: 'Berjuang demi kesempurnaan.',
+        pernyataan_3: 'Menjadi bagian tim.',
+        pernyataan_4: 'Ingin mencapai tujuan.',
+        p_1: 'i',
+        p_2: 'x',
+        p_3: 'x',
+        p_4: 'd',
+        k_1: 'i',
+        k_2: 'c',
+        k_3: 's',
+        k_4: 'x',
+    },
+    {
+        pernyataan_1: 'Menjadi frustasi.',
+        pernyataan_2: 'Memendam perasaan dalam hati.',
+        pernyataan_3: 'Menceritakan sisi kehidupan.',
+        pernyataan_4: 'Berpihak pada oposisi.',
+        p_1: 'c',
+        p_2: 's',
+        p_3: 'x',
+        p_4: 'd',
+        k_1: 'c',
+        k_2: 's',
+        k_3: 'i',
+        k_4: 'd',
+    },
+    {
+        pernyataan_1: 'Hidup, cerewet.',
+        pernyataan_2: 'Bekerja dengan cepat, tekun.',
+        pernyataan_3: 'Mencoba mempertahankan keseimbangan.',
+        pernyataan_4: 'Mencoba mengikuti aturan.',
+        p_1: 'i',
+        p_2: 'd',
+        p_3: 's',
+        p_4: 'x',
+        k_1: 'x',
+        k_2: 'd',
+        k_3: 's',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Mengatur waktu secara efisien.',
+        pernyataan_2: 'Sering terburu-buru merasa tertekan.',
+        pernyataan_3: 'Hal-hal social merupakan hal yang penting.',
+        pernyataan_4: 'Menyelesaikan apa yang telah dimulai.',
+        p_1: 'c',
+        p_2: 'd',
+        p_3: 'i',
+        p_4: 's',
+        k_1: 'x',
+        k_2: 'd',
+        k_3: 'i',
+        k_4: 's',
+    },
+    {
+        pernyataan_1: 'Menolak perubahan mendadak.',
+        pernyataan_2: 'Cenderung sering berjanji.',
+        pernyataan_3: 'Menyendiri ketika di bawah tekanan.',
+        pernyataan_4: 'Tidak takut berkelahi.',
+        p_1: 's',
+        p_2: 'i',
+        p_3: 'x',
+        p_4: 'x',
+        k_1: 'x',
+        k_2: 'i',
+        k_3: 'c',
+        k_4: 'd',
+    },
+    {
+        pernyataan_1: 'Seorang pendukung yang baik.',
+        pernyataan_2: 'Seorang pendengar yang baik.',
+        pernyataan_3: 'Seorang penganalisa yang baik.',
+        pernyataan_4: 'Seorang delegasi yang baik.',
+        p_1: 'i',
+        p_2: 's',
+        p_3: 'c',
+        p_4: 'd',
+        k_1: 'i',
+        k_2: 's',
+        k_3: 'c',
+        k_4: 'd',
+    },
+    {
+        pernyataan_1: 'Yang penting adalah hasil.',
+        pernyataan_2: 'Melakukan dengan benar, ketepatan hitungan.',
+        pernyataan_3: 'Buat menjadi menyenangkan.',
+        pernyataan_4: 'Mari melakukan bersama.',
+        p_1: 'd',
+        p_2: 'c',
+        p_3: 'x',
+        p_4: 'x',
+        k_1: 'd',
+        k_2: 'c',
+        k_3: 'i',
+        k_4: 's',
+    },
+    {
+        pernyataan_1: 'Akan melakukan tanpa kontrol diri.',
+        pernyataan_2: 'Akan membeli berdasarkan hasrat.',
+        pernyataan_3: 'Akan menunggu, tidak ada tekanan.',
+        pernyataan_4: 'Akan membelanjakan apa yang saya inginkan.',
+        p_1: 'x',
+        p_2: 'd',
+        p_3: 's',
+        p_4: 'i',
+        k_1: 'c',
+        k_2: 'd',
+        k_3: 's',
+        k_4: 'x',
+    },
+    {
+        pernyataan_1: 'Ramah mudah berteman.',
+        pernyataan_2: 'Unik, bosan dengan rutinitas.',
+        pernyataan_3: 'Aktif mengubah hal-hal.',
+        pernyataan_4: 'Menginginkan sesuatu yang pasti.',
+        p_1: 's',
+        p_2: 'x',
+        p_3: 'd',
+        p_4: 'c',
+        k_1: 'x',
+        k_2: 'i',
+        k_3: 'd',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Non konfrontasi, mengalah.',
+        pernyataan_2: 'Penuh dengan rincian.',
+        pernyataan_3: 'Perubahan pada menit terakhir.',
+        pernyataan_4: 'Penuntut, perusak.',
+        p_1: 'x',
+        p_2: 'c',
+        p_3: 'i',
+        p_4: 'd',
+        k_1: 's',
+        k_2: 'x',
+        k_3: 'i',
+        k_4: 'd',
+    },
+    {
+        pernyataan_1: 'Menginginkan kemajuan.',
+        pernyataan_2: 'Puas dengan beberapa hal, mudah puas.',
+        pernyataan_3: 'Menggambarkan perasaan secara terbuka.',
+        pernyataan_4: 'Rendah hati, sederhana.',
+        p_1: 'd',
+        p_2: 's',
+        p_3: 'i',
+        p_4: 'x',
+        k_1: 'd',
+        k_2: 'x',
+        k_3: 'x',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Tenang, pendiam.',
+        pernyataan_2: 'Bahagia, riang.',
+        pernyataan_3: 'Menyenangkan, baik.',
+        pernyataan_4: 'Tegas, berani.',
+        p_1: 'c',
+        p_2: 'i',
+        p_3: 's',
+        p_4: 'd',
+        k_1: 'c',
+        k_2: 'i',
+        k_3: 'x',
+        k_4: 'd',
+    },
+    {
+        pernyataan_1: 'Menghabiskan waktu berharga dengan orang lain.',
+        pernyataan_2: 'Merencakan masa depan, menyiapkan diri.',
+        pernyataan_3: 'Perjalanan menuju petualangan baru.',
+        pernyataan_4: 'Mendapat penghargaan jika mencapai tujuan.',
+        p_1: 's',
+        p_2: 'c',
+        p_3: 'i',
+        p_4: 'd',
+        k_1: 's',
+        k_2: 'x',
+        k_3: 'i',
+        k_4: 'd',
+    },
+    {
+        pernyataan_1: 'Peraturan perlu ditolak.',
+        pernyataan_2: 'Peraturan membuat adil.',
+        pernyataan_3: 'Peraturan membuat bosan.',
+        pernyataan_4: 'Peraturan membuat aman.',
+        p_1: 'x',
+        p_2: 'c',
+        p_3: 'i',
+        p_4: 's',
+        k_1: 'd',
+        k_2: 'x',
+        k_3: 'i',
+        k_4: 's',
+    },
+    {
+        pernyataan_1: 'Pendidikan, kebudayaan.',
+        pernyataan_2: 'Pencapaian, penghargaan.',
+        pernyataan_3: 'Keselamaatan, keamanan.',
+        pernyataan_4: 'Sosial, pertemuan kelompok.',
+        p_1: 'x',
+        p_2: 'd',
+        p_3: 's',
+        p_4: 'i',
+        k_1: 'c',
+        k_2: 'd',
+        k_3: 's',
+        k_4: 'x',
+    },
+    {
+        pernyataan_1: 'Bertanggung jawab, pendekatan langsung.',
+        pernyataan_2: 'Mudah bergaul, antusias.',
+        pernyataan_3: 'Mudah ditebak, konsisten.',
+        pernyataan_4: 'Waspada, berhati-hati.',
+        p_1: 'd',
+        p_2: 'x',
+        p_3: 'x',
+        p_4: 'c',
+        k_1: 'd',
+        k_2: 'i',
+        k_3: 's',
+        k_4: 'x',
+    },
+    {
+        pernyataan_1: 'Tidak mudah dikalahkan',
+        pernyataan_2: 'Akan melakukan sesuai perintah, mengikuti pempinan.',
+        pernyataan_3: 'Riang, ceria.',
+        pernyataan_4: 'Ingin segalanya teratur, rapi.',
+        p_1: 'd',
+        p_2: 's',
+        p_3: 'i',
+        p_4: 'x',
+        k_1: 'd',
+        k_2: 'x',
+        k_3: 'i',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Saya akan pimpin mereka.',
+        pernyataan_2: 'Saya akan mengikuti mereka.',
+        pernyataan_3: 'Saya akan bujuk mereka.',
+        pernyataan_4: 'Saya akan membujuk mereka',
+        p_1: 'd',
+        p_2: 's',
+        p_3: 'i',
+        p_4: 'c',
+        k_1: 'x',
+        k_2: 's',
+        k_3: 'i',
+        k_4: 'x',
+    },
+    {
+        pernyataan_1: 'Memikirkan orang lain dahulu.',
+        pernyataan_2: 'Kompetitif, menyukai tantangan.',
+        pernyataan_3: 'Optimis, positif.',
+        pernyataan_4: 'Berfikir logis, sistematis',
+        p_1: 's',
+        p_2: 'd',
+        p_3: 'i',
+        p_4: 'x',
+        k_1: 's',
+        k_2: 'd',
+        k_3: 'i',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Menyenagkan orang, ramah.',
+        pernyataan_2: 'Tertawa keras, hidup.',
+        pernyataan_3: 'Berani, tegas.',
+        pernyataan_4: 'Tenang, pendiam',
+        p_1: 's',
+        p_2: 'x',
+        p_3: 'd',
+        p_4: 'c',
+        k_1: 's',
+        k_2: 'i',
+        k_3: 'd',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Menginginkan kekuasaan lebih.',
+        pernyataan_2: 'Menginginkan kesempatan baru.',
+        pernyataan_3: 'Menghindari konflik apapun.',
+        pernyataan_4: 'Menginginkan arah yang jelas',
+        p_1: 'x',
+        p_2: 'i',
+        p_3: 's',
+        p_4: 'x',
+        k_1: 'd',
+        k_2: 'x',
+        k_3: 's',
+        k_4: 'c',
+    },
+    {
+        pernyataan_1: 'Bisa diandalkan, bisa digantungkan.',
+        pernyataan_2: 'Kreatif, Unik.',
+        pernyataan_3: 'Berorientasi pada hasil.',
+        pernyataan_4: 'Memegang teguh standar tinggi, akurat',
+        p_1: 'x',
+        p_2: 'i',
+        p_3: 'd',
+        p_4: 'c',
+        k_1: 's',
+        k_2: 'i',
+        k_3: 'x',
+        k_4: 'x',
+    },
+];
+
+/**=====================
+ * ===== DOM / HTML ====
+ * =====================
+ */
+
+// container
+const container = document.getElementById('container');
+
+// nomor
+const nomor_tes = document.getElementById('nomor_tes');
+
+// Jumlah / total tes
+const jml_tes = document.getElementById('jml_tes');
+
+// info petunjuk penggunaan
+const petunjuk = document.getElementById('petunjuk');
+
+// tombol tampil petunjuk
+const tampil_petunjuk = document.getElementById('tampil_petunjuk');
+
+// input paling sesuai
+const palingSesuai = document.querySelectorAll('.paling');
+
+// input kurang sesuai
+const kurangSesuai = document.querySelectorAll('.kurang');
+
+// pernyataan
+const pernyataan_1 = document.getElementById('pernyataan_1');
+const pernyataan_2 = document.getElementById('pernyataan_2');
+const pernyataan_3 = document.getElementById('pernyataan_3');
+const pernyataan_4 = document.getElementById('pernyataan_4');
+
+// tombol submit
+const tombolSubmit = document.getElementById('submit');
+
+/**==========================
+ * ===== Global Variable ====
+ * ==========================
+ */
+
+// score MOST DISC
+let mostDominance = 0;
+let mostInfluence = 0;
+let mostSteadiness = 0;
+let mostComplience = 0;
+let mostBintang = 0;
+
+// score LEAST DISC
+let leastDominance = 0;
+let leastInfluence = 0;
+let leastSteadiness = 0;
+let leastComplience = 0;
+let leastBintang = 0;
+
+// nomor quesioner
+let no_quesioner = 0;
+
+/**===================
+ * ===== Function ====
+ * ===================
+ */
+
+tampilKuis();
+
+// function tampil data
+function tampilKuis() {
+    clearRadio();
+
+    nomor_tes.innerText = `${no_quesioner + 1}`;
+    jml_tes.innerText = `${database.length}`;
+
+    const quesioner = database[no_quesioner];
+
+    pernyataan_1.innerText = quesioner.pernyataan_1;
+    pernyataan_2.innerText = quesioner.pernyataan_2;
+    pernyataan_3.innerText = quesioner.pernyataan_3;
+    pernyataan_4.innerText = quesioner.pernyataan_4;
+}
+
+// function clear pilihan
+function clearRadio() {
+    palingSesuai.forEach((paling) => {
+        paling.checked = false;
+    });
+    kurangSesuai.forEach((kurang) => {
+        kurang.checked = false;
+    });
+}
+
+// function pilihan
+function pilihanPaling() {
+    let p_sesuai = undefined;
+
+    palingSesuai.forEach((paling) => {
+        if (paling.checked) {
+            p_sesuai = paling.id;
+        }
+    });
+
+    return p_sesuai;
+}
+
+// function pilihan kurang sesuai
+function pilihanKurang() {
+    let p_kurang = undefined;
+
+    kurangSesuai.forEach((kurang) => {
+        if (kurang.checked) {
+            p_kurang = kurang.id;
+        }
+    });
+
+    return p_kurang;
+}
+
+/**=========================
+ * ===== Event Listener ====
+ * =========================
+ */
+
+// tampil_petunjuk.addEventListener('click', () => {
+//     petunjuk.classList.remove('hide');
+// });
+
+// ketika tombol klik di tekan maka
+// rekam hasil pilihan sesusai dan tidak sesuai
+
+tombolSubmit.addEventListener('click', () => {
+    let tidakSesuai = pilihanKurang();
+    let sesuai = pilihanPaling();
+
+    petunjuk.classList.add('hide');
+
+    if (sesuai && tidakSesuai) {
+        if (
+            (sesuai === 'p_1' && tidakSesuai == 'k_1') ||
+            (sesuai === 'p_2' && tidakSesuai == 'k_2') ||
+            (sesuai === 'p_3' && tidakSesuai == 'k_3') ||
+            (sesuai === 'p_4' && tidakSesuai == 'k_4')
+        ) {
+            alert('Tidak boleh memilih pernyataan yang sama');
+        } else {
+            switch (sesuai) {
+                case 'p_1':
+                    sesuai = database[no_quesioner].p_1;
+                    break;
+                case 'p_2':
+                    sesuai = database[no_quesioner].p_2;
+                    break;
+                case 'p_3':
+                    sesuai = database[no_quesioner].p_3;
+                    break;
+                case 'p_4':
+                    sesuai = database[no_quesioner].p_4;
+                    break;
+            }
+
+            switch (tidakSesuai) {
+                case 'k_1':
+                    tidakSesuai = database[no_quesioner].k_1;
+                    break;
+                case 'k_2':
+                    tidakSesuai = database[no_quesioner].k_2;
+                    break;
+                case 'k_3':
+                    tidakSesuai = database[no_quesioner].k_3;
+                    break;
+                case 'k_4':
+                    tidakSesuai = database[no_quesioner].k_4;
+                    break;
+            }
+
+            switch (sesuai) {
+                case 'd':
+                    mostDominance++;
+                    break;
+                case 'i':
+                    mostInfluence++;
+                    break;
+                case 's':
+                    mostSteadiness++;
+                    break;
+                case 'c':
+                    mostComplience++;
+                    break;
+                case 'x':
+                    mostBintang++;
+                    break;
+            }
+
+            switch (tidakSesuai) {
+                case 'd':
+                    leastDominance++;
+                    break;
+                case 'i':
+                    leastInfluence++;
+                    break;
+                case 's':
+                    leastSteadiness++;
+                    break;
+                case 'c':
+                    leastComplience++;
+                    break;
+                case 'x':
+                    leastBintang++;
+                    break;
+            }
+
+            no_quesioner++;
+        }
+
+        if (no_quesioner < database.length) {
+            tampilKuis();
+        } else {
+            container.innerHTML = `
+
+            <div class="card total">
+                <div>
+                <h3>MOST</h3>
+                <p> Total D : ${mostDominance} </p>
+                <p> Total I : ${mostInfluence} </p>
+                <p> Total S : ${mostSteadiness} </p>
+                <p> Total C : ${mostComplience} </p>
+                <p> Total * : ${mostBintang} </p>
+                <h4>Total MOST : ${
+                    mostDominance +
+                    mostInfluence +
+                    mostSteadiness +
+                    mostComplience +
+                    mostBintang
+                }</h4>
+                </div>
+                <div>
+                <h3>LEAST</h3>
+                <p> Total D : ${leastDominance} </p>
+                <p> Total I : ${leastInfluence} </p>
+                <p> Total S : ${leastSteadiness} </p>
+                <p> Total C : ${leastComplience} </p>
+                <p> Total * : ${leastBintang} </p>
+                 <h4>Total LEAST : ${
+                     leastDominance +
+                     leastInfluence +
+                     leastSteadiness +
+                     leastComplience +
+                     leastBintang
+                 }</h4>
+                </div>
+                <div>
+                <h3>CHANGE</h3>
+                <p> Total D : ${mostDominance - leastDominance} </p>
+                <p> Total I : ${mostInfluence - leastInfluence} </p>
+                <p> Total S : ${mostSteadiness - leastSteadiness} </p>
+                <p> Total C : ${mostComplience - leastComplience} </p>
+                </div>
+            </div>
+
+            <button onclick="location.reload()">Reload</button>
+
+            `;
+        }
+    } else {
+        alert(
+            'Anda harus memilih pernyataan yang "PALING" sesuai dan yang "KURANG" sesuai'
+        );
+    }
+});
